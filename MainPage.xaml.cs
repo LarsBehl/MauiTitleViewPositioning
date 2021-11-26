4cfeacfb72791b3b22220bc6da5c2ef1303aab03
Lars Behl <lars.behl@gmx.de>
2021-11-26 11:49:22 +0100

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Essentials;
+
+namespace MauiApp1
+{
+	public partial class MainPage : Shell
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
